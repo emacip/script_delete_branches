fe61ab7cb07e7c32cfcb40cf6afba2302b34703e
emacip <emacipbelmonte@gmail.com>
2019-10-24 18:10:19 +0100

    Update branches names

--- script_delete_branches.sh
+++ script_delete_branches.sh
@@ -1,4 +1,4 @@
-for branch in $(git branch -r --merged develop | grep origin | grep -v develop | grep -v release | grep -v master | sed -E "s|^ *origin/||g")
+for branch in $(git branch -r --merged branch_1 | grep origin | grep -v branch_1 | grep -v branch_2 | grep -v master | sed -E "s|^ *origin/||g")
 do
     git push origin $branch --delete
 done
